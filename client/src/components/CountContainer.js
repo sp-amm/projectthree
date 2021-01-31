296d305eb92e4b863233031e5fd03628b0962dda
sp <sam.pack.sp@gmail.com>
2021-01-31 14:31:03 +1030

    countdown timer working

--- client/src/components/CountContainer.js
+++ client/src/components/CountContainer.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import Countdown from './Countdown';
+import './style.css'
 import CountButton from './Count-btn';
 import Restart from './Restart-btn';
 import Submit from './Submit';
+import Countdown, { zeroPad } from 'react-countdown';
+ 
 
+        
+const renderer = ({ minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      return <Submit />;
+    } else {
+
+      // Render a countdown
+      return <span>{zeroPad(minutes, 2)}:{zeroPad(seconds, 2)}</span>;
+    }
+  };
 
 const CountContainer = () => {
     return(
         <div>
-            <Countdown />
+            <div className="counttime">
+            <Countdown date={Date.now() + 900000} renderer={renderer} />
+            </div>
             <CountButton />
             <Restart />
-            <Submit />
         </div>
     )
 }
